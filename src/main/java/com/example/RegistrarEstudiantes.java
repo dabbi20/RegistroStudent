242313b3b60f330ec49761933c88307b78a69b26
David Carrasco <davidack123456789@gmail.com>
2026-05-28 19:45:24 -0500

    creacion de arrays

--- src/main/java/com/example/RegistrarEstudiantes.java
+++ src/main/java/com/example/RegistrarEstudiantes.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 
 public class RegistrarEstudiantes {
 
+
+static String [] nombres = new String[100];
+static int [] edades = new int[100];
+static double [] notas = new double[100];
+static int contador = 0;
+static Scanner sc = new Scanner(System.in);
+
     static void main(String[] args) {
-        
+
     }
 }
